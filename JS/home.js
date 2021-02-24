69acf1476d8c17bacd0fd1835945a7f993b05906
SIMPLON <tellai>
2021-02-24 16:20:32 +0100

    sauvegarde home.js

--- JS/home.js
+++ JS/home.js
@@ -7,4 +7,16 @@ const url = "localhost:3051/"
 section_Python.addEventListener("click", function(){
     let argument = "sections/python/"
     url = url + argument
-})
+});
+section_Javascript.addEventListener("click", function () {
+  let argument = "sections/js/";
+  url = url + argument;
+});
+section_Cloud.addEventListener("click", function () {
+  let argument = "sections/cloud/";
+  url = url + argument;
+});
+section_Docker.addEventListener("click", function () {
+  let argument = "sections/docker/";
+  url = url + argument;
+});
