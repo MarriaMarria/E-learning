348049405eac57a68c4fa4c24915dcdf98a102ae
MarriaMarria <maria.kononevs@gmail.com>
2021-02-26 11:01:22 +0100

    saving work

--- Python/db_learning.py
+++ Python/db_learning.py
@@ -13,9 +13,9 @@ class MyDB: # I changed name as SCRAP has nothing to do with it
 
     def __init__(self):
         self.mydb = mysql.connector.connect(
-                    host="localhost", # "localhost"
+                    host="ms2", # "localhost" # remember to change HOST
                     user="root",
-                    password="pw", #pw
+                    password="password", #pw # change password
                     database= "learning3",
                     auth_plugin='mysql_native_password'
                 )
